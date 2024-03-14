ea50108f796b90e83ac8ab2543e4c527644dfa7b
jhs-panda <joy22030@gmail.com>
2024-03-14 15:55:36 -0400

    corrected name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -15,7 +15,7 @@ export default function QueryProcessor(query: string): string {
 
   if (query.toLowerCase().includes("name")) {
     return (
-      "My name is Joy Song. "
+      "Joy Song"
     );
   }
 
